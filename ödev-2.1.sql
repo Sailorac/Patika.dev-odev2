58e7880343be64cb6a3980c7e8bbe49f0d33e7a3
Sailorac <119450113+Sailorac@users.noreply.github.com>
2022-12-01 20:52:38 +0300

    Add files via upload

---
+++ ödev-2.1.sql
@@ -0,0 +1,2 @@
+SELECT * FROM film
+WHERE replacement_cost between 12.99 and 16.99
---
+++ ödev-2.3.sql
@@ -0,0 +1,3 @@
+SELECT * FROM film
+where rental_rate IN (.99, 2.99, 4.99) 
+And replacement_cost IN (12.99, 15.99, 28.99)
